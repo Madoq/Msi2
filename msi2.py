6db74b35d3df9834c8f583ec49a953513a7f5461
Mateusz Nieplowicz <58771301+Madoq@users.noreply.github.com>
2022-12-14 13:36:44 +0100

    First commit
    Msi task

---
+++ msi2.py
@@ -0,0 +1,49 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+"""\/generate database of traning data\/"""
+mnist = tf.keras.datasets.mnist
+(x_train,y_train), (x_test,y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+"""\/generate model of neural network \/"""
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+model.compile(optimizer="adam", loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+"""\/teaching model with data\/"""
+model.fit(x_train, y_train, epochs=3)
+
+"""\/save model\/"""
+model.save('handwriten.model') 
+
+"""\/load model\/"""
+model = tf.keras.models.load_model('handwriten.model')
+
+"""\/model acc and loss display\/"""
+loss, accuracy = model.evaluate(x_test,y_test)
+print(loss)
+print(accuracy)
+
+"""\/testing model on real data 28x28pixel img in digits folder\/"""
+image_number = 1
+while os.path.isfile(f"digits/digit{image_number}.png"):
+    try:
+        img = cv2.imread(f"digits/digit{image_number}.png")[:,:,0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"this digit is probablyy a {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print("wrong format")
+    finally:
+        image_number +=1
+ 
