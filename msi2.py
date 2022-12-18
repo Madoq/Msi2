164b0a25ac979341d5f5d23aa00ea414eba0814f
KozaPawel <p.koza99@o2.pl>
2022-12-18 17:29:15 +0100

    nowe zdjecia i dodany if

--- msi2.py
+++ msi2.py
@@ -3,6 +3,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+train = True
+
 """\/generate database of traning data\/"""
 mnist = tf.keras.datasets.mnist
 (x_train,y_train), (x_test,y_test) = mnist.load_data()
@@ -10,19 +12,20 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-"""\/generate model of neural network \/"""
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
-model.compile(optimizer="adam", loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+if train:
+    """\/generate model of neural network \/"""
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dense(10, activation='softmax'))
+    model.compile(optimizer="adam", loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-"""\/teaching model with data\/"""
-model.fit(x_train, y_train, epochs=3)
+    """\/teaching model with data\/"""
+    model.fit(x_train, y_train, epochs=10)
 
-"""\/save model\/"""
-model.save('handwriten.model') 
+    """\/save model\/"""
+    model.save('handwriten.model') 
 
 """\/load model\/"""
 model = tf.keras.models.load_model('handwriten.model')
